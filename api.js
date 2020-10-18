0908439287f9df1dcb3238d656d8e2f9f9fce3db
codingpalette <codingpalette@gmail.com>
2020-10-18 19:01:24 +0900

    API 작업

---
+++ api.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const KEY = "a992d1fae6c06b990f53908e421ee199";
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+
+const makeRequest = (path, params) => {
+    return axios.get(`${path}`, {
+        params: {
+            ...params,
+            api_key:KEY,
+            language: 'ko',
+        }
+    })
+}
+
+const getAnything = async (path, params = {}) => {
+    try {
+        const {data: { results }} = await makeRequest(path, params);
+        return [results, null];
+    } catch (e) {
+        return [null, e];
+    }
+};
+
+export const movieApi = {
+    nowPlaying: () => getAnything("/movie/now_playing"),
+    popular: () => getAnything("/movie/popular"),
+    upcoming: () => getAnything("/movie/upcoming", { region: "kr" }),
+    search: query => getAnything("/search/movie", { query }),
+    movie: id => getAnything(`/movie/${id}`),
+    discover: () => getAnything("/discover/movie")
+}
+
+export const tvApi = {
+    today: () => getAnything("/tv/airing_today"),
+    thisWeek: () => getAnything("/tv/on_the_air"),
+    topRated: () => getAnything("/tv/top_rated"),
+    popular: () => getAnything("/tv/popular"),
+    search: query => getAnything("/search/tv", { query }),
+    show: id => getAnything(`/tv/${id}`)
+}
--- screens/Movies.js
+++ screens/Movies.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View, Button } from 'react-native';
+import {movieApi} from "../api";
 
 
 export default ({navigation}) => {
+
+    const getData = async () => {
+        const [nowPlaying, nowPlayingError] = await movieApi.nowPlaying();
+        const [popular, popularError] = await movieApi.popular();
+    };
+    useEffect(() => {
+        getData()
+    }, [])
     return(
         <View style={{flex:1, backgroundColor:"#000"}}>
             <Text>Movies</Text>
